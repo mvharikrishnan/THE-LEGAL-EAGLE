20f870caac2c6bf7bcf817aaf920b49480c40ded
HARI KRISHNAN M V <106075831+mvharikrishnan@users.noreply.github.com>
2022-06-09 09:47:31 +0530

    Add files via upload

---
+++ adminheader.php
@@ -0,0 +1,69 @@
+<?php include 'connection.php' ;?>
+
+	
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>ADMIN</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Law Firm Website Template" name="keywords">
+        <meta content="Law Firm Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@1,600;1,700;1,800&family=Roboto:wght@400;500&display=swap" rel="stylesheet"> 
+        
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+    	<!-- Nav Bar Start -->
+            <div class="nav-bar">
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                        <a href="#" class="navbar-brand">MENU</a>
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto">
+                               
+<a href="adminhome.php" class="nav-item nav-link active">home</a>
+	<a href="adminmanagecasetype.php" class="nav-item nav-link active">Case Type</a>
+	<a href="adminmanagecourts.php" class="nav-item nav-link active">Courts</a>
+	<a href="adminmanagelawdetails.php" class="nav-item nav-link active">Law Details</a>
+	<a href="adminviewadvocate.php" class="nav-item nav-link active">Advocate</a>
+	<a href="adminviewuser.php" class="nav-item nav-link active">Users</a>
+	<a href="adminviewcomplaintsandsendreply.php" class="nav-item nav-link active">Complaints And Send Reply</a>
+	<a href="home.php" class="nav-item nav-link active">Logout</a>
+
+                                
+                                
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <!-- Nav Bar End -->
+            <!-- Carousel Start -->
+<style type="text/css">
+  div.ex1{
+    background-image: url('img/admin2.jpg');
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    max-height: auto;
+  }
+</style>
+
